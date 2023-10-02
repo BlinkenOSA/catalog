4f95e156be90b9c06cf06d1ae3287a401a7f508c
Josh <jozsef.bone@gmail.com>
2023-10-02 10:24:06 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import {useRouter} from "next/router";
 import SearchPage from "../components/pages/search/SearchPage";
 import LayoutWithFacet from "../components/layout/LayoutWithFacet";
 import {useDeepCompareEffect, useMeasure, useSessionStorage} from "react-use";
-import { Media } from "../utils/media";
+import {Media} from "../utils/media";
 import FacetPageMobile from "../components/facets/mobile/FacetPageMobile";
 import BreadcrumbSearchMobile from "../components/breadcrumbs/mobile/BreadcrumbSearchMobile";
 import {Buffer} from "buffer";
@@ -22,24 +22,24 @@ const SOLR_USER = process.env.NEXT_PUBLIC_SOLR_USER;
 const SOLR_PASS = process.env.NEXT_PUBLIC_SOLR_PASS;
 
 export async function getServerSideProps(context) {
-  const params = context.query
-  const solrParams = Object.entries(params).length > 0 ? makeSolrParams(params) : makeSolrParams({qf: 'identifier_search'})
-  let data = []
+    const params = context.query
+    const solrParams = Object.entries(params).length > 0 ? makeSolrParams(params) : makeSolrParams({qf: 'identifier_search'})
+    let data = []
 
-  if (Object.entries(params).length > 0) {
-    // SOLR Basic Authentication
-    let headers = new Headers();
-    headers.set('Authorization', 'Basic ' + Buffer.from(SOLR_USER + ":" + SOLR_PASS).toString('base64'));
+    if (Object.entries(params).length > 0) {
+        // SOLR Basic Authentication
+        let headers = new Headers();
+        headers.set('Authorization', 'Basic ' + Buffer.from(SOLR_USER + ":" + SOLR_PASS).toString('base64'));
 
-    const res = await fetch(`${SOLR_API}?` + solrParams, {
-      headers: headers
-    })
-    data = await res.json()
-  } else {
-    data = []
-  }
+        const res = await fetch(`${SOLR_API}?` + solrParams, {
+            headers: headers
+        })
+        data = await res.json()
+    } else {
+        data = []
+    }
 
-  return { props: { data } }
+    return {props: {data}}
 }
 
 
@@ -53,7 +53,7 @@ const Index = ({data, badgeData, newIsadData}) => {
     /* Keep current route in the session storage */
     const [storage, setStorage] = useSessionStorage('blinken-osa-catalog-searchpage', router.asPath);
     useDeepCompareEffect(() => {
-       setStorage(router.asPath)
+        setStorage(router.asPath)
     }, [router])
 
     const onSelectFacetGroup = (facetGroup) => {
@@ -68,104 +68,104 @@ const Index = ({data, badgeData, newIsadData}) => {
         setSelectedFacetGroup('')
     }
 
-    if (selectedFacetGroup === '') {
-        if (data.length === 0) {
-          return (
-            <IndexPageNew
-              badgeData={badgeData}
-              newIsadData={newIsadData}
-              onSelectFacetGroup={onSelectFacetGroup}
-            />
-          )
-        } else {
-          return (
-            <Layout>
-              <Head>
-                <title>Blinken OSA Archivum - Catalog</title>
-              </Head>
-              <Media lessThan="md">
-                {
-                  (className, renderChildren) => {
-                    return renderChildren ?
-                      <BreadcrumbSearchMobile
-                        total={data ? data['response']['numFound'] : 0}
-                        reference={ref}
-                        inverse={false}
-                        module={''}
-                        onSelectFacetGroup={onSelectFacetGroup}
-                        isMobile={true}
-                      /> : '';
-                  }
-                }
-              </Media>
-              <Media greaterThanOrEqual="md">
-                {
-                  (className, renderChildren) => {
-                    return renderChildren ?
-                      <BreadcrumbSearch
-                        total={data ? data['response']['numFound'] : 0}
-                        reference={ref}
-                        inverse={false}
-                        module={''}
-                        isMobile={false}
-                      /> : '';
-                  }
-                }
-              </Media>
-              <SearchPage
-                data={data}
-                onSelectFacetGroup={onSelectFacetGroup}
-              />
-            </Layout>
-          )
-        }
-    } else {
+    if (data.length === 0) {
         return (
-            <LayoutWithFacet>
-                <Head>
-                    <title>Blinken OSA Archivum - Catalog</title>
-                </Head>
-                <Media lessThan="md">
-                    {
-                        (className, renderChildren) => {
-                            return renderChildren ?
-                                <React.Fragment>
+            <IndexPageNew
+                badgeData={badgeData}
+                newIsadData={newIsadData}
+                onSelectFacetGroup={onSelectFacetGroup}
+            />
+        )
+    } else {
+        if (selectedFacetGroup === '') {
+            return (
+                <Layout>
+                    <Head>
+                        <title>Blinken OSA Archivum - Catalog</title>
+                    </Head>
+                    <Media lessThan="md">
+                        {
+                            (className, renderChildren) => {
+                                return renderChildren ?
                                     <BreadcrumbSearchMobile
-                                        reference={ref}
                                         total={data ? data['response']['numFound'] : 0}
-                                        inverse={true}
+                                        reference={ref}
+                                        inverse={false}
                                         module={''}
                                         onSelectFacetGroup={onSelectFacetGroup}
                                         isMobile={true}
-                                    />
-                                    <FacetPageMobile
-                                        breadcrumbHeight={height}
-                                        facets={data ? data['facet_counts']['facet_fields'] : {}}
-                                        total={data ? data['response']['numFound'] : 0}
-                                        onShowButtonClick={onShowButtonClick}
-                                    />
-                                </React.Fragment> : '';
+                                    /> : '';
+                            }
                         }
-                    }
-                </Media>
-                <Media greaterThanOrEqual="md">
-                    <BreadcrumbSearch
-                        reference={ref}
-                        inverse={true}
-                        module={''}
-                    />
-                    <FacetPage
-                        breadcrumbHeight={height}
-                        facets={data ? data['facet_counts']['facet_fields'] : {}}
-                        total={data ? data['response']['numFound'] : 0}
-                        selectedFacetGroup={selectedFacetGroup}
+                    </Media>
+                    <Media greaterThanOrEqual="md">
+                        {
+                            (className, renderChildren) => {
+                                return renderChildren ?
+                                    <BreadcrumbSearch
+                                        total={data ? data['response']['numFound'] : 0}
+                                        reference={ref}
+                                        inverse={false}
+                                        module={''}
+                                        isMobile={false}
+                                    /> : '';
+                            }
+                        }
+                    </Media>
+                    <SearchPage
+                        data={data}
                         onSelectFacetGroup={onSelectFacetGroup}
-                        onShowButtonClick={onShowButtonClick}
                     />
-                </Media>
-            </LayoutWithFacet>
-        )
+                </Layout>
+            )
+        } else {
+            return (
+                <LayoutWithFacet>
+                    <Head>
+                        <title>Blinken OSA Archivum - Catalog</title>
+                    </Head>
+                    <Media lessThan="md">
+                        {
+                            (className, renderChildren) => {
+                                return renderChildren ?
+                                    <React.Fragment>
+                                        <BreadcrumbSearchMobile
+                                            reference={ref}
+                                            total={data ? data['response']['numFound'] : 0}
+                                            inverse={true}
+                                            module={''}
+                                            onSelectFacetGroup={onSelectFacetGroup}
+                                            isMobile={true}
+                                        />
+                                        <FacetPageMobile
+                                            breadcrumbHeight={height}
+                                            facets={data ? data['facet_counts']['facet_fields'] : {}}
+                                            total={data ? data['response']['numFound'] : 0}
+                                            onShowButtonClick={onShowButtonClick}
+                                        />
+                                    </React.Fragment> : '';
+                            }
+                        }
+                    </Media>
+                    <Media greaterThanOrEqual="md">
+                        <BreadcrumbSearch
+                            reference={ref}
+                            inverse={true}
+                            module={''}
+                        />
+                        <FacetPage
+                            breadcrumbHeight={height}
+                            facets={data ? data['facet_counts']['facet_fields'] : {}}
+                            total={data ? data['response']['numFound'] : 0}
+                            selectedFacetGroup={selectedFacetGroup}
+                            onSelectFacetGroup={onSelectFacetGroup}
+                            onShowButtonClick={onShowButtonClick}
+                        />
+                    </Media>
+                </LayoutWithFacet>
+            )
+        }
     }
 }
 
-export default Index
+export default Index;
