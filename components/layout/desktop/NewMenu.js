965fcb25aa40a4900f9e3bcecf6c29adfdc8a4a3
JoshBone <jozsef.bone@gmail.com>
2023-10-13 10:17:04 +0200

    New Menu updates

--- components/layout/desktop/NewMenu.js
+++ components/layout/desktop/NewMenu.js
@@ -3,37 +3,47 @@ import style from "./NewMenu.module.scss";
 import { useCart } from "react-use-cart";
 import Image from "next/image";
 import {motion} from "framer-motion";
+import {useRouter} from "next/router";
 
 /**
  * Top right static menu
  */
-const NewMenu = ({onMenuOpen}) => {
+const NewMenu = () => {
+    const router = useRouter();
     const { totalUniqueItems } = useCart();
+
+    const getClass = (pathName) => {
+        if (router.pathname === pathName) {
+            return `${style.MenuItem} ${style.Active}`
+        } else {
+            return style.MenuItem
+        }
+    }
 
     return (
       <React.Fragment>
         <div className={style.MenuList}>
             <ul>
                 <motion.a href={'/archival-collections'} style={{zIndex: 1}}>
-                    <li className={style.MenuItem} style={{border: "none"}}>
+                    <li className={getClass('/archival-collections')} style={{border: "none"}}>
                         <div className={style.Image}><Image src="/icons/ArchivalUnitBlack.svg" height={20} width={20} /></div>
                         <div className={style.Text}>Archival Collections</div>
                     </li>
                 </motion.a>
                 <motion.a href={'/library-special-collections'} style={{zIndex: 2}}>
-                    <li className={style.MenuItem}>
+                    <li className={getClass('/library-special-collections')}>
                         <div className={style.Image}><Image src="/icons/BookIconBlack.svg" height={20} width={20} /></div>
                         <div className={style.Text}>Library Special Collections</div>
                     </li>
                 </motion.a>
                 <a href={'/image-gallery'} style={{zIndex: 3}}>
-                    <li className={style.MenuItem}>
+                    <li className={getClass('/image-gallery')}>
                         <div className={style.Image}><Image src="/icons/TypeStillImage.svg" height={25} width={25} /></div>
                         <div className={style.Text}>Digital Image Gallery</div>
                     </li>
                 </a>
                 <a href={'/requests'} style={{zIndex: 4}}>
-                    <li className={style.MenuItem} style={{borderRight: "1px solid #FFF"}}>
+                    <li className={getClass('/requests')} style={{borderRight: "1px solid #FFF"}}>
                         <div className={style.Image}><Image src="/icons/Cart.svg" height={15} width={15} /></div>
                         <div className={style.Text}>Requests</div>
                     </li>
