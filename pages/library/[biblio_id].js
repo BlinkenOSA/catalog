208a772683184105c2740bf125237b85907b58c1
JoshBone <jozsef.bone@gmail.com>
2025-04-08 13:30:53 +0200

    Add library redirect.

---
+++ pages/library/[biblio_id].js
@@ -0,0 +1,22 @@
+import Hashids from 'hashids'
+
+
+export async function getServerSideProps(context) {
+	const hashids = new Hashids('osalibrary', 8)
+	const {biblio_id} = context.params;
+
+	const catalogID = hashids.encode(biblio_id)
+
+	return {
+		redirect: {
+			destination: `/catalog/${catalogID}`,
+			permanent: false,
+		},
+	}
+}
+
+const BiblioRedirect = () => {
+
+}
+
+export default BiblioRedirect
