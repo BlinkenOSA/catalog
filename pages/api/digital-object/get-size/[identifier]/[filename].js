4bdd8df61ee851d1a1f55d18d7be5d5bbda6e378
Josh <jozsef.bone@gmail.com>
2025-04-09 16:26:32 +0200

    Update [filename].js

--- pages/api/digital-object/get-size/[identifier]/[filename].js
+++ pages/api/digital-object/get-size/[identifier]/[filename].js
@@ -1,12 +1,20 @@
 import {getPdfURL} from "../../../../../utils/digitalObjectFunctions";
 import axios from "axios";
 
+const CATALOG_DOWNLOAD_USER = process.env.NEXT_PUBLIC_CATALOG_DOWNLOAD_USER;
+const CATALOG_DOWNLOAD_PASS = process.env.NEXT_PUBLIC_CATALOG_DOWNLOAD_PASS;
+
 export default async function handler(req, res) {
     const {identifier, filename} = req.query;
     const url = getPdfURL(identifier, filename)
 
     const getData = () => {
-        return axios.head(url).then(response => response.headers)
+        return axios.head(url, {
+            auth: {
+                username: CATALOG_DOWNLOAD_USER,
+                password: CATALOG_DOWNLOAD_PASS
+            }
+        }).then(response => response.headers)
     }
 
     const data = await getData();
