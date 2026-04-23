ec80bc9cb3709e56cf3784aed6fef8207a11d481
JoshBone <jozsef.bone@gmail.com>
2026-04-23 13:36:23 +0200

    Update requests.js

--- pages/requests.js
+++ pages/requests.js
@@ -2,10 +2,8 @@ import React from "react";
 import style from "./pages.module.scss";
 import Layout from "../components/layout/Layout";
 import Head from "next/head";
-import BreadcrumbSearch from "../components/breadcrumbs/desktop/BreadcrumbSearch";
 import dynamic from 'next/dynamic'
 import { Media } from "../utils/media";
-import BreadcrumbSearchMobile from "../components/breadcrumbs/mobile/BreadcrumbSearchMobile";
 
 
 const RequestsPage = () => {
